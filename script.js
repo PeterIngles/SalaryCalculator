76625ff9fa0caa722d484ccebbfcfc0c0dda0764
Peter Ingles <peter.ingles@gmail.com>
2023-07-16 12:01:36 -0500

    Added handleSubmit function

---
+++ script.js
@@ -0,0 +1,28 @@
+$(document).ready(onReady);
+
+function onReady() {
+    console.log('in OnReady')
+
+    $('.submit-button').on('click', handleSubmit)
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    console.log('in handleSubmit')
+    const firstNameInput = $("#firstNameInput").val();
+    const LastNameInput = $("#LastNameInput").val();
+    const idInput = $("#idInput").val();
+    const titleInput = $("#titleInput").val();
+    const annualSalaryInput = $("#annualSalaryInput").val();
+  
+    console.log('SalaryInput', annualSalaryInput)
+    $("#tableSalaries").append(`
+    <tr id= "salaryRow">
+      <td>${firstNameInput}</td>
+      <td>${LastNameInput}</td>
+      <td>${idInput}</td>
+      <td>${titleInput}</td>
+      <td>${annualSalaryInput}</td>
+      <td><button>❌</button</td>
+    </tr> `)
+  }
